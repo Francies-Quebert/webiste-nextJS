c8149b33ed7e165817f2d75cc49d777b2816070f
Francies Fernandes <quebert88@gmail.com>
2023-05-05 22:13:14 +0100

    environment file update

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -7,7 +7,7 @@ export default function Error() {
   return (
     <div className="h-screen bg-gray-200 flex flex-col ju~stify-center items-center">
       <Image src={'@/icons/error.svg'} alt="error" className="w-56 mb-8" width={20}/>
-      <div className="bg-white px-9 py-14 shadow rounded">
+      <div className="bg-white px-9 py-14 shadow rounded text-dark-theme-text">
         <h3 className="text-3xl font-bold">Well, this is embarrassing</h3>
         <p className="text-reg font-bold">Internal Server Error</p>
         <p className="mt-6 text-sm font-light">Error Code: 400</p>
